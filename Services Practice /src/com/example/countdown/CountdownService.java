81e27a0fd1cef08b1789304136ab5904a3f262d7
Derek Bertubin <dbertubin@fullsail.edu>
2013-08-07 19:32:27 -0400

    Commented the Service Class: Now I have a fairly good grasp on what i need to do…

--- Services Practice /src/com/example/countdown/CountdownService.java
+++ Services Practice /src/com/example/countdown/CountdownService.java
@@ -17,41 +17,55 @@ import android.util.Log;
  *
  */
 
-
+// Create Countdown Service that extends IntentService 
 public class CountdownService extends IntentService{
 
+	
+	// setup keys to key constants to pass data in thru the service 
 	public static final String MESSENGER_KEY = "messenger";
 	public static final String TIME_KEY = "time";
 	
+	
 	public CountdownService() {
 		super("CountdownService");
-		// TODO Auto-generated constructor stub
-		
 		
 	}
 
 	@Override
+	
+	
 	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
-		
 		
 		Log.i("onHandleIntent", "Started");
 		
+		
+		// The bundle is the container in which the data is passed about in the application 
 		Bundle extras = intent.getExtras();
+		
+		// messenger is set by casting the value retrieved from the bundle with the key of MESSENGER_KEY
 		Messenger messenger= (Messenger) extras.get(MESSENGER_KEY);	
+		
+		// We Create a string called timer and grab the value passed in the bundle associated with the TIME_KEY
 		String timer = extras.getString(TIME_KEY);
 		
 		
-		
+		//We instantiate the init valued for the countdown int
 		int countdown = 10;
 		
+		
+		
 		try {
+			
+			// since countdown is an int and we are passing a string we need to parse out the value of the string into an int. 
+			// For this we will Integer.parseInt() and pass in the string timer 
 			countdown = Integer.parseInt(timer);
 			
 		} catch (Exception e) {
 			Log.e("onIntentHandler", e.getMessage().toString());
 		}
 		
+		
+		// The as long as the value of countdown is greater than 0 we will sleep the thread for x second interval 
 		while (countdown > 0) {
 			try {
 				Thread.sleep(1000);
@@ -70,10 +84,16 @@ public class CountdownService extends IntentService{
 		
 		Log.i("onIntentHandler", "Countdown is done!");
 		
+		
+		// once the while loop is done we create instantiate a instance of the Messenger Class and then obtain it 
 		Message message = Message.obtain();
+		
+		// we set the message to send back as Activity.RESULT_OK using .arg1 and as well as send the obj string to give us some feed back that the action is complete
 		message.arg1 = Activity.RESULT_OK;
 		message.obj = "Service is Done";
 		
+		
+		// now we use a try/catch to send the message using messenger.send() and pass the message.
 		try {
 			messenger.send(message);
 		} catch (RemoteException e) {
--- Services Practice /src/com/example/countdown/MainActivity.java
+++ Services Practice /src/com/example/countdown/MainActivity.java
@@ -50,12 +50,13 @@ public class MainActivity extends Activity {
 				return;
 				}
 				
-				// setup handler to pass message 
+				// The Handler Class is used to pass messages between background threads, such as Services, and the main application thread.
 				Handler countdownHandler = new Handler(){
 
 					@Override
 					
-					// create method to handle the message - default method of the Handler Class  
+					// The handleMessage method can be used similar to the way the onPostExecute method is used upon completion of an AsyncTask. 
+					// the handleMessage is explicitly called when the Service sends a message
 					public void handleMessage(Message msg) {
 						// TODO Auto-generated method stub
 						super.handleMessage(msg);
@@ -87,12 +88,30 @@ public class MainActivity extends Activity {
 					
 				};
 				
+				//Once you have designed your Service Class and instantiated the Handler in your launching Activity you need a way for the 
+				//Service to access that Handler so it can communicate with the Activity. 
+				//The Messenger Class in instantiated in the calling activity and passed to the Service to provide a reference to the Handler.
+				// in this case it will be "countdownHandler"
 				Messenger countdownMessenger = new Messenger(countdownHandler);
 				
+				
+				//The Activity has to command the operating system to start the Service. This operation is done through an Intent Class.
+				//The Intent Class is a container for delivering information to the Android operating system, as well as, in this case the Service being started.
+				//The Intent constructor takes as input parameters the calling Activity's context, and the Service Class to start. 
 				Intent startCountdownIntent = new Intent(_context, CountdownService.class);
 				
+				//Then the Intent is loaded with any data that is to be passed to this Service. 
+				//You can put any type of data into the intent using the "putExtra" method, where you enter two parameters. 
+				//The first parameter is a key string, giving a name to the data for the recipient to access. 
+				//The second parameter in the putExtra method is the data, which can be of any type of data, even a class such as Messenger.
+				
+				
 				startCountdownIntent.putExtra(CountdownService.MESSENGER_KEY, countdownMessenger);
+				
+				
 				startCountdownIntent.putExtra(CountdownService.TIME_KEY, inputText.getText().toString());
+				
+				// Start the service remember that the handleMessage method will not be called until the Service is done. 
 				startService(startCountdownIntent);
 				
 				resultText.setText("Waiting...");
