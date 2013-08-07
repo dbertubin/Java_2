6328779a29ff5fc6088af7e590c49ea976a6fa9b
Derek Bertubin <dbertubin@fullsail.edu>
2013-08-07 18:13:44 -0400

    Services Tut finished and working =)
    On to Project

--- Services Practice /src/com/example/countdown/CountdownService.java
+++ Services Practice /src/com/example/countdown/CountdownService.java
@@ -3,16 +3,26 @@
  */
 package com.example.countdown;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 /**
  * @author PetRockz
  *
  */
+
+
 public class CountdownService extends IntentService{
 
+	public static final String MESSENGER_KEY = "messenger";
+	public static final String TIME_KEY = "time";
+	
 	public CountdownService() {
 		super("CountdownService");
 		// TODO Auto-generated constructor stub
@@ -24,7 +34,23 @@ public class CountdownService extends IntentService{
 	protected void onHandleIntent(Intent intent) {
 		// TODO Auto-generated method stub
 		
+		
+		Log.i("onHandleIntent", "Started");
+		
+		Bundle extras = intent.getExtras();
+		Messenger messenger= (Messenger) extras.get(MESSENGER_KEY);	
+		String timer = extras.getString(TIME_KEY);
+		
+		
+		
 		int countdown = 10;
+		
+		try {
+			countdown = Integer.parseInt(timer);
+			
+		} catch (Exception e) {
+			Log.e("onIntentHandler", e.getMessage().toString());
+		}
 		
 		while (countdown > 0) {
 			try {
@@ -34,6 +60,27 @@ public class CountdownService extends IntentService{
 				Log.e("Sleep", e.getMessage().toString());
 				e.printStackTrace();
 			}
+			
+			countdown--;
+			
+			Log.i("onIntentHandler", "Counter = " + String.valueOf(countdown));
+			
+			
+		}
+		
+		Log.i("onIntentHandler", "Countdown is done!");
+		
+		Message message = Message.obtain();
+		message.arg1 = Activity.RESULT_OK;
+		message.obj = "Service is Done";
+		
+		try {
+			messenger.send(message);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			
+			Log.e("onIntentHandler", e.getMessage().toString());
+			e.printStackTrace();
 		}
 		
 	}
--- Services Practice /src/com/example/countdown/MainActivity.java
+++ Services Practice /src/com/example/countdown/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.countdown;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Messenger;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,19 +23,22 @@ public class MainActivity extends Activity {
 	TextView resultText;
 	Button startButton;
 	EditText inputText;
-
+	Context _context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		
+		_context = this;
 		resultText = (TextView) findViewById(R.id.resultText);
 		inputText = (EditText)findViewById(R.id.editText);
 		inputText.setInputType(InputType.TYPE_CLASS_NUMBER);
 		startButton = (Button)findViewById(R.id.startButton);
 		startButton.setOnClickListener(new OnClickListener() {
 			
+			@SuppressLint("HandlerLeak")
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -38,7 +46,7 @@ public class MainActivity extends Activity {
 					
 				Toast toast = Toast.makeText(getApplicationContext(), "TEXT", Toast.LENGTH_LONG);
 				toast.show();
-				
+				return;
 				}
 				
 				Handler countdownHandler = new Handler(){
@@ -48,14 +56,42 @@ public class MainActivity extends Activity {
 						// TODO Auto-generated method stub
 						super.handleMessage(msg);
 						
+						String response = null;
+						
+						
+						if(msg.arg1 == RESULT_OK && msg.obj != null){
+							
+							try {
+								response = (String) msg.obj;
+							} catch (Exception e) {
+								
+								Log.e("HANDLER_MESSAGE" , e.getMessage().toString());
+								e.printStackTrace();
+					
+							}
+							
+							resultText.setText(response);
+								
+						}
+				
 					}
 					
 				};
 				
+				Messenger countdownMessenger = new Messenger(countdownHandler);
+				
+				Intent startCountdownIntent = new Intent(_context, CountdownService.class);
+				
+				startCountdownIntent.putExtra(CountdownService.MESSENGER_KEY, countdownMessenger);
+				startCountdownIntent.putExtra(CountdownService.TIME_KEY, inputText.getText().toString());
+				startService(startCountdownIntent);
+				
+				resultText.setText("Waiting...");
+				
+				
 			}
 		});
-		
-		
+
 	}
 
 	@Override
